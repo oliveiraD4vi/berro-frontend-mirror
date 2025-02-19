401ef6907887afedabd1bf21c5f4de01851d37fe
Davi Oliveira <davi.comp.oliveira@gmail.com>
2025-02-19 17:41:54 -0300

    feat: added login page component

--- src/app/pages/auth/login/login.page.ts
+++ src/app/pages/auth/login/login.page.ts
@@ -1,15 +1,16 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonToolbar, IonButtons } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonToolbar, IonButtons, IonButton } from '@ionic/angular/standalone';
 import { AppearanceButtonComponent } from "../../../components/appearance-button/appearance-button.component";
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
   standalone: true,
-  imports: [IonButtons, IonContent, IonHeader, IonToolbar, CommonModule, FormsModule, AppearanceButtonComponent]
+  imports: [IonButton, IonButtons, IonContent, IonHeader, IonToolbar, CommonModule, FormsModule, AppearanceButtonComponent, RouterLink]
 })
 export class LoginPage {
   constructor() {}

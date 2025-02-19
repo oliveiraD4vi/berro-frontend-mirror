fe17fd2b18e575dade57c224f010b7ce56c7a34a
Davi Oliveira <davi.comp.oliveira@gmail.com>
2025-02-19 18:38:00 -0300

    feat: added auth guards to correctly redirect the user based on it's logged info

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,15 +1,19 @@
 import { Routes } from "@angular/router";
+import { isLoggedGuard } from "./guards/is-logged/is-logged.guard";
+import { isNotLoggedGuard } from "./guards/is-not-logged/is-not-logged.guard";
 
 export const routes: Routes = [
   {
     path: "",
     loadChildren: () =>
       import("./pages/auth/auth.routes").then((m) => m.routes),
+    canActivate: [isLoggedGuard],
   },
   {
     path: "users",
     loadChildren: () =>
       import("./pages/tabs/tabs.routes").then((m) => m.routes),
+    canActivate: [isNotLoggedGuard],
   },
   {
     path: "appearance",
